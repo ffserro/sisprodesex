529604f60948da56f6f6073805f786874deb2df4
ffserro <ffserro@hotmail.com>
2022-08-09 15:27:04 -0300

    Update page_2.py

--- pages/page_2.py
+++ pages/page_2.py
@@ -8,16 +8,16 @@ st.sidebar.markdown("# Page 2")
 
 DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+
 @st.cache
 def load_data(nrows):
-  data = pd.read_csv(DATA_URL, nrows=nrows)
-  lowercase = lambda x: str(x).lower()
-  data.rename(lowercase, axis=1, inplace=True)
-  data[DATE_COLUMN]=pd.to_date_time(data[DATE_COLUMN])
-  return data
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    return data
 
-data_load_state = st.text('Carregando os dados...')
+data_load_state = st.text('Loading data...')
 data = load_data(10000)
-data_load_state.text('Concluído! (using st.cache)')
-
+data_load_state.text("Done! (using st.cache)")
