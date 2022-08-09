238692715496112bfb06ff665a424405dd990fd4
ffserro <ffserro@hotmail.com>
2022-08-09 15:30:12 -0300

    Update page_2.py

--- pages/page_2.py
+++ pages/page_2.py
@@ -21,3 +21,6 @@ def load_data(nrows):
 data_load_state = st.text('Loading data...')
 data = load_data(10000)
 data_load_state.text("Done! (using st.cache)")
+
+st.subheader('Raw data')
+st.write(data)         
