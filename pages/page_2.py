7cee56934321477fbc17ddc5b9fbdcabb2d0101f
ffserro <ffserro@hotmail.com>
2022-08-09 15:39:38 -0300

    Update page_2.py

--- pages/page_2.py
+++ pages/page_2.py
@@ -22,15 +22,16 @@ data_load_state = st.text('Loading data...')
 data = load_data(10000)
 data_load_state.text("Done! (using st.cache)")
 
-st.subheader('Raw data')
-st.write(data)
+if st.checkbox('Show raw data'):
+    st.subheader('Raw data')
+    st.write(data)
 
 st.subheader('Quantidades de viagens por hora')
 hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
 
 st.bar_chart(hist_values)
 
-hour_to_filter = st.slider('Hora', 0,23)
+hour_to_filter = st.slider('hour', 0, 23, 17)
 filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
 st.subheader(f'Mapa das viagens que iniciaram às {hour_to_filter}:00')
 st.map(filtered_data)
