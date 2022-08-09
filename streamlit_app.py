26a838ad797a043a2d90c25fde831c4ee2da1420
ffserro <ffserro@hotmail.com>
2022-08-09 14:00:22 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,7 +46,7 @@ df
 st.write("Teste de planilha:")
 st.table(df)
 
-dataframe = (np.random.randn(10,20),
+dataframe = pd.DataFrame(np.random.randn(10,20),
 columns = ('col %d'%i for i in range(20)))
 st.dataframe(dataframe.style.highlight_max(axis=0))
 
