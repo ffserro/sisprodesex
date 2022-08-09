6e35efce06c841f1f7145a5b717958da63b7392a
ffserro <ffserro@hotmail.com>
2022-08-09 14:42:08 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ import math
 import pandas as pd
 import streamlit as st
 import numpy as np
+import time
 
 """
 # Welcome to Streamlit!
@@ -90,3 +91,13 @@ with right_column:
         'Chapéu seletor',
         ('Grifinória', 'Corvinal', 'Lufa-Lufa', 'Sonserina'))
     st.write(f'Você está na casa {chosen}!')
+
+'Começando uma longa computação...'
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+    latest_iteration.text(f'Iteration {i+1}')
+    bar.progress(i+1)
+    time.sleep(0.1)
+"...and now we're done!"
