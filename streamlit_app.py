72f5ffd7d903acb384973cb6e2bdd70a199cf5ec
ffserro <ffserro@hotmail.com>
2022-08-09 13:36:53 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,3 +36,8 @@ with st.echo(code_location='below'):
         .mark_circle(color='#0068c9', opacity=0.5)
         .encode(x='x:Q', y='y:Q'))
 """
+df = DataFrame({
+    'first column':[1,2,3,4],
+    'second column':[10,20,30,40]
+})
+df
