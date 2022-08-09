36cd216eb2e374fa52b2a63f2a8f3d7b7fc3c6c8
ffserro <ffserro@hotmail.com>
2022-08-09 15:20:41 -0300

    Update page_2.py

--- pages/page_2.py
+++ pages/page_2.py
@@ -14,7 +14,7 @@ def load_data(nrows):
   data = pd.read_csv(DATA_URL, nrows=nrows)
   lowercase = lambda x: str(x).lower()
   data.rename(lowercase, axis='columns', inplace=True)
-  data.[DATE_COLUMN]=pd.to_date_time(data[DATE_COLUMN])
+  data[DATE_COLUMN]=pd.to_date_time(data[DATE_COLUMN])
   return data
 
 data_load_state = st.text('Carregando os dados...')
