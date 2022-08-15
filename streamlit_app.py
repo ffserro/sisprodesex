15e430b42cc3b747ecaba4d7450df63c8f35c3f3
ffserro <ffserro@hotmail.com>
2022-08-15 15:42:29 -0300

    hop

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,8 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
+df = pd.read_csv('./bd_rosalvos.csv')
+
 st.title('Página teste do Rosalvito')
 
 with st.form('Inserir Fornecedores'):
@@ -12,3 +14,10 @@ with st.form('Inserir Fornecedores'):
     recebedor = st.text_input('Nome do recebedor')
     nip = st.text_input('NIP do recebedor')
     submeter = st.form_submit_button('Enviar')
+
+if submeter:
+    ins = pd.DataFrame([cnpj, nome_empresa, num_nf, data_nf, recebedor, nip])
+    df = pd.concat([df, ins], axis=0)
+    df.to_csv('./bd_rosalvos.csv')
+
+st.table(df)
