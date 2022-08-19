c36fd8c06547820140d9fd415270e2d33a643237
ffserro <ffserro@hotmail.com>
2022-08-19 06:31:35 -0300

    brute

--- pages/5_Teste.py
+++ pages/5_Teste.py
@@ -27,7 +27,7 @@ def main():
         state.w = w
 
         w.editor.add_tab("Card content", Card.DEFAULT_CONTENT, "plaintext")
-        w.editor.add_tab("Data grid", json.dumps(list(query), indent=2), "json")
+        w.editor.add_tab("Data grid", [{"id": 1, "lastName": "Snow", "firstName": "Jon", "age": 35}, {"id": 2, "lastName": "Lannister", "firstName": "Cersei", "age": 42}, {"id": 3, "lastName": "Lannister", "firstName": "Jaime", "age": 45}, {"id": 4, "lastName": "Stark", "firstName": "Arya", "age": 16}, {"id": 5, "lastName": "Targaryen", "firstName": "Daenerys", "age": 20}, {"id": 6, "lastName": "Melisandre", "firstName": 0, "age": 150}, {"id": 7, "lastName": "Clifford", "firstName": "Ferrara", "age": 44}, {"id": 8, "lastName": "Frances", "firstName": "Rossini", "age": 36}, {"id": 9, "lastName": "Roxie", "firstName": "Harvey", "age": 65}], indent=2), "json")
         w.editor.add_tab("Radar chart", json.dumps(Radar.DEFAULT_DATA, indent=2), "json")
         w.editor.add_tab("Pie chart", json.dumps(Pie.DEFAULT_DATA, indent=2), "json")
     else:
