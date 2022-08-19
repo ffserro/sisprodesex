77850ff79a20e69dd202f3a990da76375a36483c
ffserro <ffserro@hotmail.com>
2022-08-19 16:10:07 -0300

    .

--- pages/3_Envio.py
+++ pages/3_Envio.py
@@ -1,12 +1,7 @@
 import streamlit as st
 from streamlit_app import db
-from distutils import errors
-from distutils.log import error
 import streamlit as st
 import pandas as pd 
-import numpy as np
-import altair as alt
-from itertools import cycle
 
 from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode
 
