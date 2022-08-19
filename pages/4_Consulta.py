b13fb47a9d752f121f72f3f7eb7f5797f4a2c88c
ffserro <ffserro@hotmail.com>
2022-08-19 13:16:38 -0300

    .

--- pages/4_Consulta.py
+++ pages/4_Consulta.py
@@ -147,6 +147,7 @@ df_final = grid_response['data']
 selected = grid_response['selected_rows']
 selected_df = pd.DataFrame(selected)
 
+st.write(AgGrid(df))
 st.write(df_final)
 st.write(selected)
 st.write(selected_df)
