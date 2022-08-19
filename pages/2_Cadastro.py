287d412937382e73182a1ea7fa3537ea9febb3b7
ffserro <ffserro@hotmail.com>
2022-08-19 16:27:49 -0300

    usuário no banco de dados

--- pages/2_Cadastro.py
+++ pages/2_Cadastro.py
@@ -15,6 +15,11 @@ if st.session_state['authentication_status'] == True:
             st.session_state['message'] = ''
         with st.form("Cadastro", clear_on_submit=True):
             st.write("Novo Usuário")
+            f1, f2 = st.columns([1,1])
+            with f1:
+                novo_usuario = st.text_input('Insira um nome de usuário', key='usr')
+            with f2:
+                indicativo = st.text_input('Insira o indicativo da OM', key='iom')
             novo_email = st.text_input('Insira o email', key='eml')
             nova_senha = st.text_input('Insira a senha', type='password', key='snh')
             repetir_nova_senha = st.text_input('Repita a senha', type='password', key='nvs')
@@ -24,10 +29,17 @@ if st.session_state['authentication_status'] == True:
             if enviado:
                 if '@' not in novo_email:
                     st.warning('Insira um email válido')
+                elif novo_usuario in [i['usuario'] for i in list(db.child('usuarios').get().val().values())]:
+                    st.warning('Usuário já existe. Por favor escolha um novo nome de usuário.')
                 elif nova_senha != repetir_nova_senha:
                     st.warning('As senhas não conferem')
                 else:
                     st.session_state['message'] = 'Cadastro realizado com sucesso'
+                    db.child('usuarios').push({
+                        'email':novo_email,
+                        'origem':indicativo,
+                        'usuario':novo_usuario
+                    })
                     auth.create_user_with_email_and_password(novo_email, nova_senha)
                     nav_page('Cadastro')
 
