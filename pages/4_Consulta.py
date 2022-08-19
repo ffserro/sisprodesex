774d2ff621b3c2ffaf4fe2b48c78654db551eb58
ffserro <ffserro@hotmail.com>
2022-08-18 22:23:46 -0300

    selectable data

--- pages/4_Consulta.py
+++ pages/4_Consulta.py
@@ -2,11 +2,18 @@ import pandas as pd
 import streamlit as st
 import streamlit.components.v1 as components
 from streamlit_app import db
-st.write(1)
 df_itens = pd.DataFrame(columns=['pi', 'nome', 'descricao', 'lvad', 'data_envio', 'preco_unitario', 'quantidade', 'situacao', 'uf'])
-st.write(2)
 query = db.child('itens').order_by_child('situacao').equal_to('cadastrado').get().val().values()
-st.write(3)
 for i in query:
 	df_itens = pd.concat([df_itens, pd.DataFrame({x:[i[x]] for x in i})],ignore_index=True)
 st.write(df_itens)
+
+_selectable_data_table = components.declare_component(
+    "selectable_data_table")
+
+def selectable_data_table(data, key=None):
+    return _selectable_data_table(data=data, default=[], key=key)
+
+rows = selectable_data_table(df_itens)
+if rows:
+    st.write("You have selected", rows)
