33eb60b5c6dbbd22448acc29cd32a5ae10ed934c
ffserro <ffserro@hotmail.com>
2022-08-22 13:03:17 -0300

    .

--- pages/1_Principal.py
+++ pages/1_Principal.py
@@ -39,7 +39,7 @@ else:
             webbrowser.open_new_tab('https://google.com')
     with f4:
         if st.button('Sugestões do grupo'):
-            webbrowser.open_new_tab('https://firebasestorage.googleapis.com/v0/b/prodesex-8e59f.appspot.com/o/Documentos%2FNorma%20Grupo%20H.docx.pdf?alt=media&token=e61f5f01-d2d9-4527-bb4e-07540ce22348')    with f5:
+            webbrowser.open_new_tab('https://firebasestorage.googleapis.com/v0/b/prodesex-8e59f.appspot.com/o/Documentos%2FNorma%20Grupo%20H.docx.pdf?alt=media&token=e61f5f01-d2d9-4527-bb4e-07540ce22348')
     with f5:    
         st.write('')
 
