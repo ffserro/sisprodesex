3e75fc6d53dd3a63e78fae749851026283c5fd26
ffserro <ffserro@hotmail.com>
2022-08-22 01:39:48 -0300

    .

--- pages/5_Relatório.py
+++ pages/5_Relatório.py
@@ -69,66 +69,6 @@ else:
         value='R$ {:.2f}'.format(sum(df_itens.preco_unitario * df_itens.quantidade)).replace('.',',')
     )
 
-'''
-    with elements("nivo_charts"):
-
-        # Streamlit Elements includes 45 dataviz components powered by Nivo.
-
-        from streamlit_elements import nivo
-
-        DATA = [
-            { "taste": "fruity", "chardonay": 93, "carmenere": 61, "syrah": 114 },
-            { "taste": "bitter", "chardonay": 91, "carmenere": 37, "syrah": 72 },
-            { "taste": "heavy", "chardonay": 56, "carmenere": 95, "syrah": 99 },
-            { "taste": "strong", "chardonay": 64, "carmenere": 90, "syrah": 30 },
-            { "taste": "sunny", "chardonay": 119, "carmenere": 94, "syrah": 103 },
-        ]
-
-        with mui.Box(sx={"height": 500}):
-            nivo.Pie(
-                    data=df_itens, id = 'id', value='quantidade', width=300, height=300,
-                    #keys=[ "chardonay", "carmenere", "syrah" ],
-                    #indexBy="taste",
-                    #valueFormat=">-.2f",
-                    #margin={ "top": 70, "right": 80, "bottom": 40, "left": 80 },
-                    #borderColor={ "from": "color" },
-                    #gridLabelOffset=36,
-                    #dotSize=10,
-                    #dotColor={ "theme": "background" },
-                    #dotBorderWidth=2,
-                    #motionConfig="wobbly",
-                    #legends=[
-                    #    {
-                    #        "anchor": "top-left",
-                    #        "direction": "column",
-                    #        "translateX": -50,
-                    #        "translateY": -40,
-                    #        "itemWidth": 80,
-                    #        "itemHeight": 20,
-                    #        "itemTextColor": "#999",
-                    #        "symbolSize": 12,
-                    #        "symbolShape": "circle",
-                    #        "effects": [
-                    #            {
-                    #                "on": "hover",
-                    #                "style": {
-                    #                    "itemTextColor": "#000"
-                    #                }
-                    #            }
-                    #        ]
-                    #    }
-                    #],
-                    #theme={
-                    #    "background": "#FFFFFF",
-                    #    "textColor": "#31333F",
-                    #    "tooltip": {
-                    #        "container": {
-                    #            "background": "#FFFFFF",
-                    #            "color": "#31333F",
-                    #        }
-                    #    }
-                    #}
-                )'''
     st.dataframe(df_itens.set_index('id'))
 
     df_xlsx = to_excel(df_itens[['data_cadastro', 'pi', 'nome', 'descricao', 'preco_unitario', 'quantidade', 'uf', 'lvad', 'situacao', 'origem', 'data_envio', 'data_recebimento']])
