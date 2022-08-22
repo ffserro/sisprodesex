e727f5f73c39d3e3784e19366c4ed51db4a52f59
ffserro <ffserro@hotmail.com>
2022-08-22 01:06:48 -0300

    .

--- pages/5_Relatório.py
+++ pages/5_Relatório.py
@@ -51,7 +51,7 @@ else:
 
     kpi3.metric(
         label="Total de Excessos Destinados",
-        value=f'R$ {sum(df_itens.preco_unitario * df_itens.quantidade)}'
+        value='R$ {:.2f}'.format(sum(df_itens.preco_unitario * df_itens.quantidade))
     )
 
     st.dataframe(df_itens.set_index('id'))
