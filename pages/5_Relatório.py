2e1038bc60b86a96cde6dff5c9873b2e391a54ec
ffserro <ffserro@hotmail.com>
2022-08-22 01:36:38 -0300

    .

--- pages/5_Relatório.py
+++ pages/5_Relatório.py
@@ -85,50 +85,50 @@ else:
         ]
 
         with mui.Box(sx={"height": 500}):
-        nivo.Pie(
-                data=DATA, width=300, height=300,
-                #keys=[ "chardonay", "carmenere", "syrah" ],
-                #indexBy="taste",
-                #valueFormat=">-.2f",
-                #margin={ "top": 70, "right": 80, "bottom": 40, "left": 80 },
-                #borderColor={ "from": "color" },
-                #gridLabelOffset=36,
-                #dotSize=10,
-                #dotColor={ "theme": "background" },
-                #dotBorderWidth=2,
-                #motionConfig="wobbly",
-                #legends=[
-                #    {
-                #        "anchor": "top-left",
-                #        "direction": "column",
-                #        "translateX": -50,
-                #        "translateY": -40,
-                #        "itemWidth": 80,
-                #        "itemHeight": 20,
-                #        "itemTextColor": "#999",
-                #        "symbolSize": 12,
-                #        "symbolShape": "circle",
-                #        "effects": [
-                #            {
-                #                "on": "hover",
-                #                "style": {
-                #                    "itemTextColor": "#000"
-                #                }
-                #            }
-                #        ]
-                #    }
-                #],
-                #theme={
-                #    "background": "#FFFFFF",
-                #    "textColor": "#31333F",
-                #    "tooltip": {
-                #        "container": {
-                #            "background": "#FFFFFF",
-                #            "color": "#31333F",
-                #        }
-                #    }
-                #}
-            )
+            nivo.Pie(
+                    data=DATA, width=300, height=300,
+                    #keys=[ "chardonay", "carmenere", "syrah" ],
+                    #indexBy="taste",
+                    #valueFormat=">-.2f",
+                    #margin={ "top": 70, "right": 80, "bottom": 40, "left": 80 },
+                    #borderColor={ "from": "color" },
+                    #gridLabelOffset=36,
+                    #dotSize=10,
+                    #dotColor={ "theme": "background" },
+                    #dotBorderWidth=2,
+                    #motionConfig="wobbly",
+                    #legends=[
+                    #    {
+                    #        "anchor": "top-left",
+                    #        "direction": "column",
+                    #        "translateX": -50,
+                    #        "translateY": -40,
+                    #        "itemWidth": 80,
+                    #        "itemHeight": 20,
+                    #        "itemTextColor": "#999",
+                    #        "symbolSize": 12,
+                    #        "symbolShape": "circle",
+                    #        "effects": [
+                    #            {
+                    #                "on": "hover",
+                    #                "style": {
+                    #                    "itemTextColor": "#000"
+                    #                }
+                    #            }
+                    #        ]
+                    #    }
+                    #],
+                    #theme={
+                    #    "background": "#FFFFFF",
+                    #    "textColor": "#31333F",
+                    #    "tooltip": {
+                    #        "container": {
+                    #            "background": "#FFFFFF",
+                    #            "color": "#31333F",
+                    #        }
+                    #    }
+                    #}
+                )
     st.dataframe(df_itens.set_index('id'))
 
     df_xlsx = to_excel(df_itens[['data_cadastro', 'pi', 'nome', 'descricao', 'preco_unitario', 'quantidade', 'uf', 'lvad', 'situacao', 'origem', 'data_envio', 'data_recebimento']])
