9a6d679279d8bd418f110d9cc607df5c00673fa0
ffserro <ffserro@hotmail.com>
2022-08-21 23:47:03 -0300

    .

--- pages/4_Consulta.py
+++ pages/4_Consulta.py
@@ -1,12 +1,8 @@
 import streamlit as st
 from Logout import db, auth
-from distutils import errors
-from distutils.log import error
 import streamlit as st
 import pandas as pd 
 import numpy as np
-import altair as alt
-from itertools import cycle
 from utilidades import nav_page
 from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode
 
