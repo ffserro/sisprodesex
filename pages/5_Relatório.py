7cd09824b1125ae6607c6f2e081c343f5904cb9d
ffserro <ffserro@hotmail.com>
2022-08-22 00:09:10 -0300

    .

--- pages/5_Relatório.py
+++ pages/5_Relatório.py
@@ -5,30 +5,34 @@ from io import BytesIO
 from pyxlsb import open_workbook as open_xlsb
 from datetime import datetime
 
+st.set_page_config(page_title='SISPRODESEX', page_icon='https://www.marinha.mil.br/sites/default/files/favicon-logomarca-mb.ico', layout="centered", initial_sidebar_state="expanded", menu_items=None)
 
-def to_excel(df):
-    output = BytesIO()
-    writer = pd.ExcelWriter(output, engine='xlsxwriter')
-    df.to_excel(writer, index=False, sheet_name='Sheet1')
-    workbook = writer.book
-    worksheet = writer.sheets['Sheet1']
-    format1 = workbook.add_format({'num_format': '0.00'}) 
-    worksheet.set_column('A:A', None, format1)  
-    writer.save()
-    processed_data = output.getvalue()
-    return processed_data
+if st.session_state['authentication_status'] != True or 'authentication_status' not in st.session_state:
+    nav_page('')
+else:
+    def to_excel(df):
+        output = BytesIO()
+        writer = pd.ExcelWriter(output, engine='xlsxwriter')
+        df.to_excel(writer, index=False, sheet_name='Sheet1')
+        workbook = writer.book
+        worksheet = writer.sheets['Sheet1']
+        format1 = workbook.add_format({'num_format': '0.00'}) 
+        worksheet.set_column('A:A', None, format1)  
+        writer.save()
+        processed_data = output.getvalue()
+        return processed_data
 
-query = db.child('itens').get().val().values()
+    query = db.child('itens').get().val().values()
 
-df_itens = pd.DataFrame()
-for i in query:
-    df_itens = pd.concat([df_itens, pd.DataFrame({x:[i[x]] for x in i})],ignore_index=True)
+    df_itens = pd.DataFrame()
+    for i in query:
+        df_itens = pd.concat([df_itens, pd.DataFrame({x:[i[x]] for x in i})],ignore_index=True)
 
-st.title('Emitir Relatório')
+    st.title('Emitir Relatório')
 
-st.dataframe(df_itens)
+    st.dataframe(df_itens)
 
-df_xlsx = to_excel(df_itens)
-st.download_button(label='Baixar relatório',
-                                data=df_xlsx ,
-                                file_name= f'Relatorio{datetime.now().year}{datetime.now().month}{datetime.now().day}.xlsx')
+    df_xlsx = to_excel(df_itens)
+    st.download_button(label='Baixar relatório',
+                                    data=df_xlsx ,
+                                    file_name= f'Relatorio{datetime.now().year}{datetime.now().month}{datetime.now().day}.xlsx')
