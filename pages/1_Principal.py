42ae0b511c822987e84a438ee9693a70dd645f01
ffserro <ffserro@hotmail.com>
2022-08-22 10:46:58 -0300

    .

--- pages/1_Principal.py
+++ pages/1_Principal.py
@@ -3,6 +3,7 @@ from Logout import auth
 from utilidades import nav_page
 import pandas as pd
 import numpy as np
+import webbrowser
 st.set_page_config(page_title='SISPRODESEX', page_icon='https://www.marinha.mil.br/sites/default/files/favicon-logomarca-mb.ico', layout="centered", initial_sidebar_state="expanded", menu_items=None)
 
 if st.session_state['authentication_status'] != True or 'authentication_status' not in st.session_state:
@@ -26,7 +27,14 @@ else:
     st.write('''
     Esperamos com esse trabalho contribuir com a eficiência do SAbM e também caminhar no sentido de consolidar a cultura Data-driven no contexto administrativo da Marinha do Brasil.
     ''')
-
+    st.write('''Confira abaixo as normas que regem o SAbM e as sugestões formuladas pelo grupo:''')
+    f1, f2 = st.columns(2)
+    with f1:
+        if st.button('SGM-401'):
+            webbrowser.open_new_tab('https://firebasestorage.googleapis.com/v0/b/prodesex-8e59f.appspot.com/o/Documentos%2FSGM-401.pdf?alt=media&token=55abeb30-8758-4db2-8052-bcfab7cbde00')
+    with f2:
+        if st.button('Sugestões do grupo'):
+            webbrowser.open_new_tab('https://firebasestorage.googleapis.com/v0/b/prodesex-8e59f.appspot.com/o/Documentos%2FNorma%20Grupo%20H.docx.pdf?alt=media&token=e61f5f01-d2d9-4527-bb4e-07540ce22348')
 
 
 
