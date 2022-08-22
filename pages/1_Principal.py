ede6179634ba1f14dc40a27f9cae8c610d544262
ffserro <ffserro@hotmail.com>
2022-08-21 23:25:23 -0300

    .

--- pages/1_Principal.py
+++ pages/1_Principal.py
@@ -10,7 +10,16 @@ if st.session_state['authentication_status'] != True or 'authentication_status' 
 else:
     st.markdown("# Seja bem vindo ao PRODESEX")
     st.sidebar.markdown("# Página principal")
-    
+    st.sidebar.write('Para navegar pelas funcionalidades do sistema, por favor clique no link para ser redirecionado para umas das abas que são mostradas acima.')
+
+    st.write('''
+    O Sistema de Apoio ao Programa de Destinação de Excessos é uma sugestão de melhoria para os processos de intendência do Grupo H do CApA-2022, composto pelos:
+    - 1T(IM) Sacramento
+    - 1T(IM) Raí
+    - 1T(IM) Matheus Bispo
+    - 1T(IM) Sêrro
+    - 1T(QC-IM) Ludmilla
+    ''')
 
 
 
@@ -36,30 +45,4 @@ else:
 
 
 
-    DATE_COLUMN = 'date/time'
-    DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-                'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
-    @st.cache
-    def load_data(nrows):
-        data = pd.read_csv(DATA_URL, nrows=nrows)
-        lowercase = lambda x: str(x).lower()
-        data.rename(lowercase, axis='columns', inplace=True)
-        data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-        return data
 
-    data_load_state = st.text('Loading data...')
-    data = load_data(10000)
-    data_load_state.text("Done! (using st.cache)")
-    
-    if st.checkbox('Show raw data'):
-        st.subheader('Raw data')
-        st.write(data)
-
-    st.subheader('Quantidades de viagens por hora')
-    hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-    st.bar_chart(hist_values)
-
-    hour_to_filter = st.slider('hour', 0, 23, 17)
-    filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
-    st.subheader(f'Mapa das viagens que iniciaram às {hour_to_filter}:00')
-    st.map(filtered_data)
