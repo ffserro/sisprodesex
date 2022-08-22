e85521cc869f24dda44f03a4543cb3e4afb5d285
ffserro <ffserro@hotmail.com>
2022-08-21 23:59:14 -0300

    .

--- pages/5_Relatório.py
+++ pages/5_Relatório.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from Logout import db, auth
 import pandas as pd
+from datetime import datetime
 
 query = db.child('itens').get().val().values()
 
@@ -11,3 +12,6 @@ for i in query:
 st.title('Emitir Relatório')
 
 st.dataframe(df_itens)
+
+with f as df_itens.to_excel():
+    st.download_button('Baixar relatório', f, f'Relatorio{datetime.now().year}{datetime.now().month}{datetime.now().day}.xls')
