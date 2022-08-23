144e696d713114e4c0ef63129ec1d8e33737a497
ffserro <ffserro@hotmail.com>
2022-08-22 23:17:03 -0300

    .

--- pages/1_Principal.py
+++ pages/1_Principal.py
@@ -31,7 +31,7 @@ else:
     f1, f2, f3 = st.columns(3)
     with f1:
         st.write(f'''
-            <a target="_self" href="https://firebasestorage.googleapis.com/v0/b/prodesex-8e59f.appspot.com/o/Documentos%2FSGM-401.pdf?alt=media&token=55abeb30-8758-4db2-8052-bcfab7cbde00">
+            <a target="_blank" href="https://firebasestorage.googleapis.com/v0/b/prodesex-8e59f.appspot.com/o/Documentos%2FSGM-401.pdf?alt=media&token=55abeb30-8758-4db2-8052-bcfab7cbde00">
                 <button>
                     SGM-201
                 </button>
