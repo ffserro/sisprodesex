cadc2dff225e196212808b62eca09a72523e8166
ffserro <ffserro@hotmail.com>
2022-08-22 23:29:31 -0300

    .

--- pages/1_Principal.py
+++ pages/1_Principal.py
@@ -3,7 +3,7 @@ from Logout import auth
 from utilidades import nav_page
 import pandas as pd
 import numpy as np
-import webbrowser
+from link_button import link_button
 st.set_page_config(page_title='SISPRODESEX', page_icon='https://www.marinha.mil.br/sites/default/files/favicon-logomarca-mb.ico', layout="centered", initial_sidebar_state="expanded", menu_items=None)
 
 if st.session_state['authentication_status'] != True or 'authentication_status' not in st.session_state:
@@ -41,11 +41,12 @@ else:
             unsafe_allow_html=True
         )
     with f2:
-        if st.button('ABASTCMARINST'):
-            webbrowser.open('https://google.com', new=2)
+        link_button('ABASTCMARINST', 'https://google.com')
+        
     with f3:
         if st.button('Sugestões do grupo'):
-            webbrowser.open_new_tab('https://firebasestorage.googleapis.com/v0/b/prodesex-8e59f.appspot.com/o/Documentos%2FNorma%20Grupo%20H.docx.pdf?alt=media&token=e61f5f01-d2d9-4527-bb4e-07540ce22348')
+            pass
+            #webbrowser.open_new_tab('https://firebasestorage.googleapis.com/v0/b/prodesex-8e59f.appspot.com/o/Documentos%2FNorma%20Grupo%20H.docx.pdf?alt=media&token=e61f5f01-d2d9-4527-bb4e-07540ce22348')
 
 
 
