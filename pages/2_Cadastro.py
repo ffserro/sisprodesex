8fb603ada0019e444c70ff6ca95154d4fc576f46
ffserro <ffserro@hotmail.com>
2022-08-23 00:11:01 -0300

    .

--- pages/2_Cadastro.py
+++ pages/2_Cadastro.py
@@ -8,41 +8,54 @@ if st.session_state['authentication_status'] != True or 'authentication_status' 
     nav_page('')
 else:
     if st.session_state['username'] == 'admin':
-        st.markdown("# Cadastro de usuários")
+        
         st.sidebar.markdown("# Cadastro")
         st.sidebar.write('Esta página é exclusiva do administrador do sistema para inclusão de usuários')
+        servico = st.sidebar.selectbox('Por favor selecione o serviço desejado:',['Cadastro', 'Alteração de dados'])
+
         if 'message' in st.session_state and st.session_state['message'] != '':
             st.success(st.session_state['message'])
             st.session_state['message'] = ''
-        with st.form("Cadastro", clear_on_submit=True):
-            st.write("Novo Usuário")
-            f1, f2 = st.columns([1,1])
-            with f1:
-                novo_usuario = st.text_input('Insira um nome de usuário', key='usr')
-            with f2:
-                indicativo = st.text_input('Insira o indicativo da OM', key='iom')
-            novo_email = st.text_input('Insira o email', key='eml')
-            nova_senha = st.text_input('Insira a senha', type='password', key='snh')
-            repetir_nova_senha = st.text_input('Repita a senha', type='password', key='nvs')
+
+        def cadastro():
+            st.markdown("# Cadastro de usuários")
+            with st.form("Cadastro", clear_on_submit=True):
+                st.write("Novo Usuário")
+                f1, f2 = st.columns([1,1])
+                with f1:
+                    novo_usuario = st.text_input('Insira um nome de usuário', key='usr')
+                with f2:
+                    indicativo = st.text_input('Insira o indicativo da OM', key='iom')
+                novo_email = st.text_input('Insira o email', key='eml')
+                nova_senha = st.text_input('Insira a senha', type='password', key='snh')
+                repetir_nova_senha = st.text_input('Repita a senha', type='password', key='nvs')
 
 
-            enviado = st.form_submit_button("Enviar")
-            if enviado:
-                if '@' not in novo_email:
-                    st.warning('Insira um email válido')
-                elif novo_usuario in [i['usuario'] for i in list(db.child('usuarios').get().val().values())]:
-                    st.warning('Usuário já existe. Por favor escolha um novo nome de usuário.')
-                elif nova_senha != repetir_nova_senha:
-                    st.warning('As senhas não conferem')
-                else:
-                    st.session_state['message'] = 'Cadastro realizado com sucesso'
-                    db.child('usuarios').push({
-                        'email':novo_email,
-                        'origem':indicativo.upper(),
-                        'usuario':novo_usuario
-                    })
-                    auth.create_user_with_email_and_password(novo_email, nova_senha)
-                    nav_page('Cadastro')
+                enviado = st.form_submit_button("Enviar")
+                if enviado:
+                    if '@' not in novo_email:
+                        st.warning('Insira um email válido')
+                    elif novo_usuario in [i['usuario'] for i in list(db.child('usuarios').get().val().values())]:
+                        st.warning('Usuário já existe. Por favor escolha um novo nome de usuário.')
+                    elif nova_senha != repetir_nova_senha:
+                        st.warning('As senhas não conferem')
+                    else:
+                        st.session_state['message'] = 'Cadastro realizado com sucesso'
+                        db.child('usuarios').push({
+                            'email':novo_email,
+                            'origem':indicativo.upper(),
+                            'usuario':novo_usuario
+                        })
+                        auth.create_user_with_email_and_password(novo_email, nova_senha)
+                        nav_page('Cadastro')
+
+        def altera_dados():
+            pass
+
+        if servico == 'Cadastro':
+            cadastro()
+        elif servico == 'Alteração de dados':
+            altera_dados()
 
     else:
         st.markdown("# Cadastro de itens")
