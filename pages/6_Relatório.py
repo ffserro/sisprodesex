eccb94bb4d0c519a2f4dbecf32440a0f40473750
ffserro <ffserro@hotmail.com>
2022-08-24 00:20:16 -0300

    .

--- pages/6_Relatório.py
+++ pages/6_Relatório.py
@@ -115,28 +115,4 @@ else:
     df_itens = pd.DataFrame(grid_response['data'])
     df_xlsx = to_excel(df_itens[['data_cadastro', 'pi', 'nome', 'descricao', 'preco_unitario', 'quantidade', 'uf', 'lvad', 'situacao', 'origem', 'data_envio', 'data_recebimento']])
 
-
-
-
-
-
-    '''
-    df_itens['ano'] = df_itens.data_cadastro.str[-4:]
-
-    c1, c2 = st.columns(2)
-
-    with c1:
-        filtro = st.selectbox('Selecione um filtro:', ['-','Origem', 'Ano', 'Situação'])
-        filtro = {'-':'-','Origem':'origem', 'Ano':'ano', 'Situação':'situacao'}[filtro]
-    with c2:
-        valor = st.selectbox('Igual a:', (df_itens[filtro].unique() if filtro != '-' else '-'))
-    if filtro != '-':
-        st.dataframe(df_itens.set_index('id')[df_itens[filtro] == valor].drop(columns=['ano']))
-
-        df_xlsx = to_excel(df_itens[['data_cadastro', 'pi', 'nome', 'descricao', 'preco_unitario', 'quantidade', 'uf', 'lvad', 'situacao', 'origem', 'data_envio', 'data_recebimento']][df_itens[filtro] == valor])
-    else:
-        st.dataframe(df_itens.set_index('id').drop(columns=['ano']))
-
-        df_xlsx = to_excel(df_itens[['data_cadastro', 'pi', 'nome', 'descricao', 'preco_unitario', 'quantidade', 'uf', 'lvad', 'situacao', 'origem', 'data_envio', 'data_recebimento']])
-    '''
     st.download_button(label='Baixar relatório', data=df_xlsx , file_name= f'Relatorio{datetime.now().year}{datetime.now().month}{datetime.now().day}.xlsx')
