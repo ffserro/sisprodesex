bc951448b02fa8bc8fe0f4f41a5de02d47e156d6
ffserro <ffserro@hotmail.com>
2022-08-24 13:23:37 -0300

    .

--- pages/5_Recebimento.py
+++ pages/5_Recebimento.py
@@ -14,71 +14,188 @@ elif st.session_state['origem'] != 'DEPSMRJ':
 else:
     st.sidebar.title('Recebimento')
     st.sidebar.write('Esta página se destina à confirmação do recebimento por parte do DepSMRJ dos itens que se encontravam em trânsito.')
-    st.sidebar.selectbox('Selecione a funcionalidade desejada:', ['Recebimento', 'Descaracterização', 'Venda'])
+    modulo = st.sidebar.selectbox('Selecione a funcionalidade desejada:', ['Recebimento', 'Descaracterização', 'Venda'])
     
     query = db.child('itens').get().val().values()
 
     df_itens = pd.DataFrame()
     for i in query:
         df_itens = pd.concat([df_itens, pd.DataFrame({x:[i[x]] for x in i})],ignore_index=True)
-    #df_itens = df_itens.set_index('id')
-    print(df_itens)
 
-    df = df_itens[df_itens.situacao == 'Em trânsito']
+    if modulo == 'Recebimento':
+        df = df_itens[df_itens.situacao == 'Em trânsito']
 
-    if len(df) == 0:
-        st.title('Parece que não existem itens aguardando para serem recebidos pelo DepSMRJ...')
-        st.write('Por favor acompanhe o andamento dos itens enviados na aba "Consulta".')
-        st.stop()
+        if len(df) == 0:
+            st.title('Parece que não existem itens aguardando para serem recebidos pelo DepSMRJ...')
+            st.write('Por favor acompanhe o andamento dos itens enviados na aba "Consulta".')
+            st.stop()
 
-    #Infer basic colDefs from dataframe types
-    gb = GridOptionsBuilder.from_dataframe(df[['data_envio', 'pi', 'nome', 'descricao', 'preco_unitario', 'quantidade', 'uf', 'lvad', 'situacao', 'origem']])
+        #Infer basic colDefs from dataframe types
+        gb = GridOptionsBuilder.from_dataframe(df[['data_envio', 'pi', 'nome', 'descricao', 'preco_unitario', 'quantidade', 'uf', 'lvad', 'situacao', 'origem']])
 
-    #customize gridOptions
-    gb.configure_default_column(maintainColumnOrder=True, groupable=True, value=True, enableRowGroup=True, aggFunc='sum', editable=False)
-    gb.configure_auto_height(True)
-    gb.configure_pagination()
-    gb.configure_column("data_envio", 'Data', type=["dateColumnFilter","customDateTimeFormat"], custom_format_string='dd-MM-yyyy', pivot=True)
-    gb.configure_column("pi", 'PI')
-    gb.configure_column("nome", 'Nome do item')
-    gb.configure_column("descricao",'Descrição')
-    gb.configure_column("lvad", 'LVAD')
-    gb.configure_column("preco_unitario", 'Preço Unitário', type=["customCurrencyFormat"], custom_currency_symbol="R$", aggFunc='sum')
-    gb.configure_column("quantidade", 'Quantidade', type=["numericColumn"], aggFunc='max')
-    gb.configure_column("situacao", 'Situação')
-    gb.configure_column("uf", 'UF')
-    gb.configure_column("origem",'Origem')
-
-
-    gb.configure_side_bar()
-
-    gb.configure_selection('multiple')
-    gb.configure_selection('multiple', use_checkbox=True, groupSelectsChildren=True, groupSelectsFiltered=True)
-
-    gb.configure_pagination(paginationAutoPageSize=True)
-
-    gb.configure_grid_options(domLayout='normal')
-    gridOptions = gb.build()
+        #customize gridOptions
+        gb.configure_default_column(maintainColumnOrder=True, groupable=True, value=True, enableRowGroup=True, aggFunc='sum', editable=False)
+        gb.configure_auto_height(True)
+        gb.configure_pagination()
+        gb.configure_column("data_envio", 'Data', type=["dateColumnFilter","customDateTimeFormat"], custom_format_string='dd-MM-yyyy', pivot=True)
+        gb.configure_column("pi", 'PI')
+        gb.configure_column("nome", 'Nome do item')
+        gb.configure_column("descricao",'Descrição')
+        gb.configure_column("lvad", 'LVAD')
+        gb.configure_column("preco_unitario", 'Preço Unitário', type=["customCurrencyFormat"], custom_currency_symbol="R$", aggFunc='sum')
+        gb.configure_column("quantidade", 'Quantidade', type=["numericColumn"], aggFunc='max')
+        gb.configure_column("situacao", 'Situação')
+        gb.configure_column("uf", 'UF')
+        gb.configure_column("origem",'Origem')
 
 
+        gb.configure_side_bar()
 
-    st.write('# Recebimento dos itens enviados')
+        gb.configure_selection('multiple')
+        gb.configure_selection('multiple', use_checkbox=True, groupSelectsChildren=True, groupSelectsFiltered=True)
 
-    grid_response = AgGrid(
-        df, 
-        gridOptions=gridOptions,
-        fit_columns_on_grid_load = True,
-        data_return_mode='FILTERED', 
-        update_mode='GRID_CHANGED',
-        allow_unsafe_jscode=True, #Set it to True to allow jsfunction to be injected
-        theme='streamlit'    
-        )
+        gb.configure_pagination(paginationAutoPageSize=True)
+
+        gb.configure_grid_options(domLayout='normal')
+        gridOptions = gb.build()
 
 
-    enviar = st.button('Receber itens enviados')
 
-    if enviar:
-        ids = [i['id'] for i in grid_response['selected_rows']]
-        for i in ([list(db.child('itens').order_by_child('id').equal_to(x).get().val().keys())[0] for x in ids]):
-            db.child('itens').child(i).update({'situacao':'Recebido', 'data_recebimento':datetime.now().strftime("%d/%m/%Y")})
-        nav_page('Recebimento')
+        st.write('# Recebimento dos itens enviados')
+
+        grid_response = AgGrid(
+            df, 
+            gridOptions=gridOptions,
+            fit_columns_on_grid_load = True,
+            data_return_mode='FILTERED', 
+            update_mode='GRID_CHANGED',
+            allow_unsafe_jscode=True, #Set it to True to allow jsfunction to be injected
+            theme='streamlit'    
+            )
+
+
+        enviar = st.button('Receber itens enviados')
+
+        if enviar:
+            ids = [i['id'] for i in grid_response['selected_rows']]
+            for i in ([list(db.child('itens').order_by_child('id').equal_to(x).get().val().keys())[0] for x in ids]):
+                db.child('itens').child(i).update({'situacao':'Recebido', 'data_recebimento':datetime.now().strftime("%d/%m/%Y")})
+            nav_page('Recebimento')
+    elif modulo == 'Descaracterização':
+        df = df_itens[df_itens.situacao == 'Recebido']
+
+        if len(df) == 0:
+            st.title('Parece que não existem itens aguardando para serem recebidos pelo DepSMRJ...')
+            st.write('Por favor acompanhe o andamento dos itens enviados na aba "Consulta".')
+            st.stop()
+
+        #Infer basic colDefs from dataframe types
+        gb = GridOptionsBuilder.from_dataframe(df[['data_envio', 'pi', 'nome', 'descricao', 'preco_unitario', 'quantidade', 'uf', 'lvad', 'situacao', 'origem']])
+
+        #customize gridOptions
+        gb.configure_default_column(maintainColumnOrder=True, groupable=True, value=True, enableRowGroup=True, aggFunc='sum', editable=False)
+        gb.configure_auto_height(True)
+        gb.configure_pagination()
+        gb.configure_column("data_envio", 'Data', type=["dateColumnFilter","customDateTimeFormat"], custom_format_string='dd-MM-yyyy', pivot=True)
+        gb.configure_column("pi", 'PI')
+        gb.configure_column("nome", 'Nome do item')
+        gb.configure_column("descricao",'Descrição')
+        gb.configure_column("lvad", 'LVAD')
+        gb.configure_column("preco_unitario", 'Preço Unitário', type=["customCurrencyFormat"], custom_currency_symbol="R$", aggFunc='sum')
+        gb.configure_column("quantidade", 'Quantidade', type=["numericColumn"], aggFunc='max')
+        gb.configure_column("situacao", 'Situação')
+        gb.configure_column("uf", 'UF')
+        gb.configure_column("origem",'Origem')
+
+
+        gb.configure_side_bar()
+
+        gb.configure_selection('multiple')
+        gb.configure_selection('multiple', use_checkbox=True, groupSelectsChildren=True, groupSelectsFiltered=True)
+
+        gb.configure_pagination(paginationAutoPageSize=True)
+
+        gb.configure_grid_options(domLayout='normal')
+        gridOptions = gb.build()
+
+
+
+        st.write('# Recebimento dos itens enviados')
+
+        grid_response = AgGrid(
+            df, 
+            gridOptions=gridOptions,
+            fit_columns_on_grid_load = True,
+            data_return_mode='FILTERED', 
+            update_mode='GRID_CHANGED',
+            allow_unsafe_jscode=True, #Set it to True to allow jsfunction to be injected
+            theme='streamlit'    
+            )
+
+
+        enviar = st.button('Descaracterizar itens recebidos')
+
+        if enviar:
+            ids = [i['id'] for i in grid_response['selected_rows']]
+            for i in ([list(db.child('itens').order_by_child('id').equal_to(x).get().val().keys())[0] for x in ids]):
+                db.child('itens').child(i).update({'situacao':'Descaracterizado', 'data_recebimento':datetime.now().strftime("%d/%m/%Y")})
+            nav_page('Recebimento')
+    elif modulo == 'Venda':
+        df = df_itens[df_itens.situacao == 'Descaracterizado']
+
+        if len(df) == 0:
+            st.title('Parece que não existem itens aguardando para serem recebidos pelo DepSMRJ...')
+            st.write('Por favor acompanhe o andamento dos itens enviados na aba "Consulta".')
+            st.stop()
+
+        #Infer basic colDefs from dataframe types
+        gb = GridOptionsBuilder.from_dataframe(df[['data_envio', 'pi', 'nome', 'descricao', 'preco_unitario', 'quantidade', 'uf', 'lvad', 'situacao', 'origem']])
+
+        #customize gridOptions
+        gb.configure_default_column(maintainColumnOrder=True, groupable=True, value=True, enableRowGroup=True, aggFunc='sum', editable=False)
+        gb.configure_auto_height(True)
+        gb.configure_pagination()
+        gb.configure_column("data_envio", 'Data', type=["dateColumnFilter","customDateTimeFormat"], custom_format_string='dd-MM-yyyy', pivot=True)
+        gb.configure_column("pi", 'PI')
+        gb.configure_column("nome", 'Nome do item')
+        gb.configure_column("descricao",'Descrição')
+        gb.configure_column("lvad", 'LVAD')
+        gb.configure_column("preco_unitario", 'Preço Unitário', type=["customCurrencyFormat"], custom_currency_symbol="R$", aggFunc='sum')
+        gb.configure_column("quantidade", 'Quantidade', type=["numericColumn"], aggFunc='max')
+        gb.configure_column("situacao", 'Situação')
+        gb.configure_column("uf", 'UF')
+        gb.configure_column("origem",'Origem')
+
+
+        gb.configure_side_bar()
+
+        gb.configure_selection('multiple')
+        gb.configure_selection('multiple', use_checkbox=True, groupSelectsChildren=True, groupSelectsFiltered=True)
+
+        gb.configure_pagination(paginationAutoPageSize=True)
+
+        gb.configure_grid_options(domLayout='normal')
+        gridOptions = gb.build()
+
+
+
+        st.write('# Recebimento dos itens enviados')
+
+        grid_response = AgGrid(
+            df, 
+            gridOptions=gridOptions,
+            fit_columns_on_grid_load = True,
+            data_return_mode='FILTERED', 
+            update_mode='GRID_CHANGED',
+            allow_unsafe_jscode=True, #Set it to True to allow jsfunction to be injected
+            theme='streamlit'    
+            )
+
+
+        enviar = st.button('Vender itens descaracterizados')
+
+        if enviar:
+            ids = [i['id'] for i in grid_response['selected_rows']]
+            for i in ([list(db.child('itens').order_by_child('id').equal_to(x).get().val().keys())[0] for x in ids]):
+                db.child('itens').child(i).update({'situacao':'Vendido', 'data_recebimento':datetime.now().strftime("%d/%m/%Y")})
+            nav_page('Recebimento')
