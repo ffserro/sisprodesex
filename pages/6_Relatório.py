6f51918dc96493bb1aa3f0465730208fec06c1c4
ffserro <ffserro@hotmail.com>
2022-08-24 00:20:37 -0300

    .

--- pages/6_Relatório.py
+++ pages/6_Relatório.py
@@ -110,8 +110,6 @@ else:
         theme='streamlit'    
         )
 
-
-    st.write(grid_response['data'])
     df_itens = pd.DataFrame(grid_response['data'])
     df_xlsx = to_excel(df_itens[['data_cadastro', 'pi', 'nome', 'descricao', 'preco_unitario', 'quantidade', 'uf', 'lvad', 'situacao', 'origem', 'data_envio', 'data_recebimento']])
 
