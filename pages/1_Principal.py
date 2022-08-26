3e355b61e491edc4a31d66097cf7cbefd27633d2
ffserro <ffserro@hotmail.com>
2022-08-26 16:09:50 -0300

    .

--- pages/1_Principal.py
+++ pages/1_Principal.py
@@ -31,6 +31,7 @@ else:
     with f1:
         st.write(f'''
             <a target="_blank" 
+            style="text-decoration:none"
             href="https://firebasestorage.googleapis.com/v0/b/prodesex-8e59f.appspot.com/o/Documentos%2FSGM-201-Rev-7.pdf?alt=media&token=63e2fece-7f2f-4b11-ac33-51728282bdef">
                 <button class = "css-1cpxqw2 edgvbvh5">
                     SGM-201
@@ -41,7 +42,8 @@ else:
         )
     with f2:
         st.write(f'''
-            <a target="_blank" 
+            <a target="_blank"
+            style="text-decoration:none"
             href="https://firebasestorage.googleapis.com/v0/b/prodesex-8e59f.appspot.com/o/Documentos%2FABASTCMARINST-20-01A---ANEXO-D---Sobressalentes.pdf?alt=media&token=243f6c51-5f46-484b-8fdd-584716f740b8">
                 <button class = "css-1cpxqw2 edgvbvh5">
                     ABASTCMARINST
@@ -53,6 +55,7 @@ else:
     with f3:
         st.write(f'''
             <a target="_blank" 
+            style="text-decoration:none"
             href="https://firebasestorage.googleapis.com/v0/b/prodesex-8e59f.appspot.com/o/Documentos%2FCorpo%20do%20Trabalho.pdf?alt=media&token=5a26fa6b-b7a7-4e4d-adee-867fc88c162d">
                 <button class = "css-1cpxqw2 edgvbvh5">
                     Sugestões do grupo
