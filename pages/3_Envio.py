face445fc7d0444a59b1136ee9fc7ee7d94fd293
ffserro <ffserro@hotmail.com>
2022-08-26 17:10:25 -0300

    .

--- pages/3_Envio.py
+++ pages/3_Envio.py
@@ -26,7 +26,7 @@ else:
     df = df[df.origem == st.session_state['origem']]
 
     if len(df) == 0:
-        st.title('Parece que não existem itens aguardando para serem enviados ao DepSMRJ...')
+        st.title('Não existem itens aguardando para serem enviados ao DepSMRJ')
         st.write('Por favor, se dirija a aba "Cadastro" para inserir novos itens, ou acompanhe o andamento dos itens enviados na aba "Consulta".')
         st.stop()
 
