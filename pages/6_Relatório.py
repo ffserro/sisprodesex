c701439fef862b4b8266087d8726cdf581b894cd
ffserro <ffserro@hotmail.com>
2022-08-26 11:13:13 -0300

    .

--- pages/6_Relatório.py
+++ pages/6_Relatório.py
@@ -1,13 +1,11 @@
 import streamlit as st
 from Logout import db, auth
 import pandas as pd
-from pandas.api.types import is_categorical_dtype, is_datetime64_any_dtype, is_numeric_dtype, is_object_dtype
 from streamlit_elements import elements, mui
 from io import BytesIO
 from pyxlsb import open_workbook as open_xlsb
 from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode
 from datetime import datetime
-from utilidades import nav_page
 
 st.set_page_config(page_title='SISPRODESEX', page_icon='https://www.marinha.mil.br/sites/default/files/favicon-logomarca-mb.ico', layout="wide", initial_sidebar_state="collapsed", menu_items=None)
 
@@ -20,74 +18,6 @@ else:
     st.sidebar.write('''
     Esta página é destinada a emissão de relatórios no formato .xlsx (excel), que consolidam as informações de todos os Órgãos de Distribuição e do DepSMRJ.
     ''')
-
-
-    def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
-    
-        modify = st.checkbox("Add filters")
-
-        if not modify:
-            return df
-
-        df = df
-
-        # Try to convert datetimes into a standard format (datetime, no timezone)
-        for col in df.columns:
-            if is_object_dtype(df[col]):
-                try:
-                    df[col] = pd.to_datetime(df[col])
-                except Exception:
-                    pass
-
-            if is_datetime64_any_dtype(df[col]):
-                df[col] = df[col].dt.tz_localize(None)
-
-        modification_container = st.container()
-
-        with modification_container:
-            to_filter_columns = st.multiselect("Filter dataframe on", df.columns)
-            for column in to_filter_columns:
-                left, right = st.columns((1, 20))
-                # Treat columns with < 10 unique values as categorical
-                if is_categorical_dtype(df[column]) or df[column].nunique() < 10:
-                    user_cat_input = right.multiselect(
-                        f"Values for {column}",
-                        df[column].unique(),
-                        default=list(df[column].unique()),
-                    )
-                    df = df[df[column].isin(user_cat_input)]
-                elif is_numeric_dtype(df[column]):
-                    _min = float(df[column].min())
-                    _max = float(df[column].max())
-                    step = (_max - _min) / 100
-                    user_num_input = right.slider(
-                        f"Values for {column}",
-                        min_value=_min,
-                        max_value=_max,
-                        value=(_min, _max),
-                        step=step,
-                    )
-                    df = df[df[column].between(*user_num_input)]
-                elif is_datetime64_any_dtype(df[column]):
-                    user_date_input = right.date_input(
-                        f"Values for {column}",
-                        value=(
-                            df[column].min(),
-                            df[column].max(),
-                        ),
-                    )
-                    if len(user_date_input) == 2:
-                        user_date_input = tuple(map(pd.to_datetime, user_date_input))
-                        start_date, end_date = user_date_input
-                        df = df.loc[df[column].between(start_date, end_date)]
-                else:
-                    user_text_input = right.text_input(
-                        f"Substring or regex in {column}",
-                    )
-                    if user_text_input:
-                        df = df[df[column].astype(str).str.contains(user_text_input)]
-
-        return df
 
     def to_excel(df):
         output = BytesIO()
@@ -140,9 +70,7 @@ else:
         value='R$ {:.2f}'.format(sum(df_itens.preco_unitario * df_itens.quantidade)).replace('.',',')
     )
 
-    df_itens = filter_dataframe(df_itens)
-
-    gb = GridOptionsBuilder.from_dataframe(df_itens)
+    gb = GridOptionsBuilder.from_dataframe(df_itens[['data_cadastro', 'pi', 'nome', 'descricao', 'preco_unitario', 'quantidade', 'uf', 'lvad', 'situacao', 'origem', 'data_envio', 'data_recebimento']])
 
     #customize gridOptions
     gb.configure_default_column(maintainColumnOrder=True, groupable=True, value=True, enableRowGroup=True, aggFunc='sum', editable=False)
@@ -172,7 +100,6 @@ else:
     gb.configure_grid_options(domLayout='normal')
     gridOptions = gb.build()
 
-    df_itens = df_itens[['pi', 'nome', 'descricao', 'lvad', 'origem', 'preco_unitario', 'quantidade', 'situacao', 'data_cadastro']]
     grid_response = AgGrid(
         df_itens, 
         gridOptions=gridOptions,
@@ -185,6 +112,6 @@ else:
 
 
     df_itens = pd.DataFrame(grid_response['data'])
-    df_xlsx = to_excel(df_itens)
+    df_xlsx = to_excel(df_itens[['data_cadastro', 'pi', 'nome', 'descricao', 'preco_unitario', 'quantidade', 'uf', 'lvad', 'situacao', 'origem', 'data_envio', 'data_recebimento', 'data_em_descaracterizacao', 'data_descaracterizado','data_alienacao', 'num_lote', 'num_leilao', 'nome_om']])
 
     st.download_button(label='Baixar relatório', data=df_xlsx , file_name= f'Relatorio{datetime.now().year}{datetime.now().month}{datetime.now().day}.xlsx')
