484da638be46ceef58a55efa5893bb2a0f76b751
ffserro <ffserro@hotmail.com>
2022-08-26 17:09:01 -0300

    .

--- pages/3_Envio.py
+++ pages/3_Envio.py
@@ -37,7 +37,7 @@ else:
     gb.configure_default_column(maintainColumnOrder=True, groupable=True, value=True, enableRowGroup=True, aggFunc='sum', editable=False)
     gb.configure_auto_height(True)
     gb.configure_pagination()
-    gb.configure_column("data_envio", 'Data', type=["dateColumnFilter","customDateTimeFormat"], custom_format_string='dd-MM-yyyy', pivot=True)
+    gb.configure_column("data_cadastro", 'Data', type=["dateColumnFilter","customDateTimeFormat"], custom_format_string='dd-MM-yyyy', pivot=True)
     gb.configure_column("pi", 'PI')
     gb.configure_column("nome", 'Nome do item')
     gb.configure_column("descricao",'Descrição')
